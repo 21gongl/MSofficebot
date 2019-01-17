da4fd69254b0ba9931d9d7dd455348fd30f78ff5
21gongl <45847537+21gongl@users.noreply.github.com>
2019-01-17 19:57:21 +0100

    Update MSofficebot.py

--- MSofficebot.py
+++ MSofficebot.py
@@ -1,10 +1,9 @@
 from nltk.chat.util import Chat, reflections
 import re
 import random
-import teacher_room_number
+from teacher_room_number import teacher_rooms
 
 # === This is the extension code for the NLTK library ===
-#        === You dont have to understand it ===
 
 class ContextChat(Chat):
     def respond(self, str):
@@ -49,113 +48,17 @@ class ContextChat(Chat):
 
 # === Your code should go here ===
 
-teacher_rooms = {
-    "Jonica Asteros":"H214b" ,
-    "Michael Barrett":"M123" ,
-    "Carl Beach":"H005" ,
-    "Kathryn Bechdolt":"AA104" ,
-    "David Berntson":"M019" ,
-    "Laura Berntson":"H004" ,
-    "Cathy Beyers":"H101" ,
-    "Gosia Gebert-Bielska": "M001" ,
-    "Adam Campbell":"C154" ,
-    "Chris Celinski":"AA103" ,
-    "Cecelia Cienska":"A004" ,
-    "Brent Chisholm":"AA007b" ,
-    "Shayne Cokerdem":"C106" ,
-    "Diane Cokerdem-DePriest":"H113" ,
-    "Gina Cuthbert":"H007b" ,
-    "Jason Cuthbert":"A003" ,
-    "Angela Dachpian":"H214b" ,
-    "David Dachpian":"H015" ,
-    "Patricia Deo":"M012" ,
-    "Suzanne Doering":"H013" ,
-    "Janice Doiron":"M013" ,
-    "Marilyn Dypczynski":"E045" ,
-    "Tamara Fernandez": "AA118" ,
-    "Joanna Galek":"C152" ,
-    "Adam Gasiejewski":"H122" ,
-    "Mark Gaspersich":"M112" ,
-    "Kevin Hanners":"M122" ,
-    "Nickie Hansen":"H111" ,
-    "Tiffany Hay":"M116" ,
-    "Ruth Hemerka":"M111" ,
-    "Erin Herold":"M115" ,
-    "Patricia Hermes":"H014" ,
-    "Lee Hodin":"H211, H212, H112",
-    "Malgorzata Hydzik": "H015",
-    "Alan James":"C105",
-    "Joanna Jaroslawska-Luft":"C102",
-    "Carol Jordan":"A002",
-    "Jenn Jordan":"AA119",
-    "Doug Julien":"C161",
-    "Agnieszka Kielcz":"H213",
-    "Agata Kielczewska":"M002" ,
-    "Caroline Kililea":"M115" ,
-    "Betsy Kirkpatrick": "H211" ,
-    "Aleksandra Kucinska": "M003" ,
-    "Hania Kula": "M003" ,
-    "Bart Kryger": "M004" ,
-    "Wiola Leszczynska": "M003" ,
-    "Samantha Linehan": "M022" ,
-    "Melissa Lyons": "AA003" ,
-    "Julie MacKay": "AA007D" ,
-    "Bill MacKenty": "H121" ,
-    "Richard Martens": "M121" ,
-    "Lada Martens-Bensova": "C161" ,
-    "Jaime Martinez": "H115" ,
-    "Stewart Merritt": "H212" ,
-    "Thomas Merritt": "AA1005" ,
-    "Stephen Miele": "C153" ,
-    "Anna Milewska": "H002" ,
-    "Maria Milewska": "H007" ,
-    "Tim Munnerlyn": "H007c" ,
-    "Valerie Navarro": "AA107" ,
-    "Michael Nieman": "A002" ,
-    "Joanna Olczak": "H202" ,
-    "Nicolas Pavlos": "C105" ,
-    "Lucy Pawlik": "M115" ,
-    "Jon Pitale": "H124" ,
-    "Laura Pitale": "H114" ,                   
-    "Pawel Ptak": "AA007b" ,
-    "Tami Ranado": "H023" ,
-    "Amber Russel": "M023, C154" ,
-    "Benjamin Schlief": "AA007b and all gyms" ,
-    "Lourdes Segurado": "M011" ,
-    "Irene Sendra": "C122" ,
-    "Solomon Senrick": "M114" ,
-    "Michael Sheehan": "H002" ,
-    "Kristin Sheehan": "M014" ,
-    "Joey Shousky": "H112" ,                   
-    "Stephen Sidaway": "A003" ,
-    "Noel Simon": "H214a" ,                   
-    "Rachella Simon": "C123" , 
-    "Josh Skjold": "H021" ,
-    "Iza Skoczylas": "M005" , 
-    "Marcin Skoczylas": "H214a" ,
-    "Ewa Smutek-Rusek": "C162" ,
-    "Carla Staffa": "C122" ,
-    "David Stein": "C107" ,
-    "Katie Stein": "AA106" ,
-    "David Stutz": "M021" ,
-    "Christopher Taylor": "H024" ,                   
-    "Elizabeth Swanson": "H101" ,
-    "Bolek Szuter": "E048-2" ,
-    "Monika Wieczorkiewicz": "H112" ,                   
-    "Kasia Wodnicka": "H002" ,
-    "Steven Wood": "C161" ,
-    "James Wyatt": "H022"                    
-}
-
-def find_teacher_room(teacher):
-    return teacher_rooms[teacher]
-
 school_bus_time = ["3:45", "4:30", "5:30"]
 
 lunch_time = {
     "not wednesday": ["11:00", "11:40"], 
     "wednesday": ["13:35", "14:20"]
     }
+
+def find_teacher_room(teacher):
+    return teacher_rooms[teacher]
+
+today_date = ""
 
 pairs = [
     [
@@ -176,7 +79,11 @@ pairs = [
     ],
     [
         r'(where is)(.*)(room?)',
-        [lambda matches: "It is in " + str( find_teacher_room(matches[1].strip()) )]
+        [lambda matches: "It is in " + str( find_teacher_room(matches[1].strip()) )] #credits: Michał
+    ],
+    [
+        r'(what day is it?)',
+        ['Today is {0}'.format(today_date)]
     ],
     [
       	r'(where is MS Lost and Found?)',
@@ -209,7 +116,7 @@ pairs = [
   	[
         r'(.*)',
         ["Maybe you should try to rephrase your question."],
-    ],
+    ]
 ]
 
 if __name__ == "__main__":
